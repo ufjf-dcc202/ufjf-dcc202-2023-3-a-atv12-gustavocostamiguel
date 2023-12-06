928de4a0c7fe0a4a1777028653a4d3bbe94fa080
gustavo costa <gustavocosta.miguel@estudante.ufjf.br>
2023-12-06 20:59:33 -0300

    finaliza atividade 12

---
+++ tests/main.js
@@ -0,0 +1,13 @@
+document.entrada.addEventListener('submit',leFormulario);
+const olJoao = document.querySelector('#olJoao');
+const olJoao = document.querySelector('#olMaria');
+
+function leFormulario(event)
+{
+   event.preventDefault(); 
+const fruta = document.entrada.fruta.value;
+const quantidade = document.entrada.quantidade.value;
+
+console.log(`fruta: ${fruta} qtd ${quantidade}`);
+document.entrada.submit();
+}
